007c476cfc249e7b4083ef3420f14b2390932c72
dakbill <dakbill@yahoo.com>
2012-06-20 11:28:33 +0000

    Lab 1

---
+++ zeller.py
@@ -0,0 +1,16 @@
+
+print "Kofi Dwomoh-Ababio\nWelcome to birthpress"
+Fname=  raw_input("Please enter your first name :")
+Sname=  raw_input("Please enter your last name :")
+print "Please enter your date of birth??"
+month=int(raw_input("Month(eg. 02,01,12...):"));year=raw_input("Year(eg. 2012...):");day=raw_input("day(eg. 31,22...):")
+A=month-3
+B=day
+C=(year%100)
+D=
+if(month==1 or month==2):
+    A=month+10
+    pyear=int(raw_input("Enter previous year: "));
+weekday=""
+
+print Fname+" "+Sname+" was born on a "+weekday
