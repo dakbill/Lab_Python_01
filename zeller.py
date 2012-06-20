c76228c16ad1215120d65e24d21c6dd06a2a7c8a
dakbill <dakbill@yahoo.com>
2012-06-20 13:19:00 +0000

    Lab 1

--- zeller.py
+++ zeller.py
@@ -3,14 +3,44 @@ print "Kofi Dwomoh-Ababio\nWelcome to birthpress"
 Fname=  raw_input("Please enter your first name :")
 Sname=  raw_input("Please enter your last name :")
 print "Please enter your date of birth??"
-month=int(raw_input("Month(eg. 02,01,12...):"));year=raw_input("Year(eg. 2012...):");day=raw_input("day(eg. 31,22...):")
-A=month-3
-B=day
-C=(year%100)
-D=
+month=int(raw_input("Month(eg. 02,01,12...):"));year=raw_input("Year(eg. 2012...):");day=int(raw_input("day(eg. 31,22...):"))
 if(month==1 or month==2):
-    A=month+10
+    Fakemonth=month+10
+else:
+    Fakemonth=month-2
+if(Fakemonth==11 or Fakemonth==12):
     pyear=int(raw_input("Enter previous year: "));
+else:
+    pyear=int(year);
 weekday=""
+A=Fakemonth
+B=day
+C=(pyear%100)
+"""if(Fakemonth==11 or Fakemonth==12):
+    C=C-1;"""
+
+D=int(pyear/100)
+W = (13*A - 1) / 5
+X=C/4
+Y=D/4
+Z = W + X + Y + B + C - 2*D
+R = Z%7
+if(R<0):R=R+7;
+if(R==0):
+    weekday="Sunday"
+elif(R==1):
+    weekday="Monday"
+elif(R==2):
+    weekday="Tuesday"
+elif(R==3):
+    weekday="Wednesday"
+elif(R==4):
+    weekday="Thursday"
+elif(R==5):
+    weekday="Friday"
+elif(R==6):
+    weekday="Saturday"
+
+    
 
 print Fname+" "+Sname+" was born on a "+weekday
