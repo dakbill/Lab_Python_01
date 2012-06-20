d296e0a91d38bacb23877cea9c52285b05b95418
dakbill <dakbill@yahoo.com>
2012-06-20 10:50:37 +0000

    Lab 1

---
+++ userinput.py
@@ -0,0 +1,7 @@
+
+print "Kofi Dwomoh-Ababio\nWelcome to birthpress"
+Fname=  raw_input("Please enter your first name :")
+Sname=  raw_input("Please enter your last name :")
+print "Please enter your date of birth??"
+month=raw_input("Month(eg. Feb,Jan...):");year=raw_input("Year(eg. 2012...):");day=raw_input("day(eg. 31,22...):")
+print Fname+" "+Sname+" was born on "+month+" "+day+", "+year+"."
